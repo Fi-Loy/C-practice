e5d3f32cefe99deb2ae913c0e63ea99190a7796e
David <david.brunner00@gmail.com>
2025-08-09 16:08:46 -0400

    Chapter 2.9: Exercise 2-6
    Write a function setbits(x, p, n, y) that moves a section of bits n long from position p, and relocates it to position y

--- main.c
+++ main.c
@@ -1,21 +1,43 @@
 //Note to next session:
-//Working on Chapter 3.5 Loops - While and For
-//I skipped the 2.9 and 2.10 exercises, but those would be good for understanding how to work with bits in C
+//Working on Chapter 3.6 Loops - Do While
+//Chapter 2.9: Exercise 2-6. 
+/* Write a function setbits(x,p,n,y) that returns x with
+** with the n bits that begin at position p set to the 
+** rightmost n bits of y, leaving the other bits unchanged.
+*/
 
 #include <stdio.h>
 
+unsigned getbits(unsigned x, int p, int n);
+unsigned setbits(unsigned x, int p, int n, int y);
+
 int main(){
 
-    int bit1 = 0;
-    int bit2 = 2;
+    int x = 143013; //143013 -> 141989
 
-    printf("%d\n", bit2<<2);
-    printf("%d", bit1 && bit2);
+    printf("%d -> %d\n", x, setbits(x,7,4,13));
 
+} 
+
+unsigned getbits(unsigned x, int p, int n){
+    return (x >> (p+1-n)) & ~(~0 << n); //(x,4,3)
 }
 
+unsigned setbits(unsigned x, int p, int n, int y){
+    int getSection = getbits(x, p, n);
+
+    return (x | (~(~0 << n) << (y-n+1))) & (~(~(~0 << n) << (y-n+1)) | (getSection << (y-n+1)));
+    //works, its ugly, but it works
+    //*it works for this instance, haven't tested any other case*
+}
+
+//2.9 Exercises
+//2.10 Exercises
 //Exercises to return to
 //3.3 else-if statements
 //sorting array algorithm
 //binary search algorithm
 //hexdump
+//3.5 shell sort
+//3.5 exercises
+//3.5 reverse function
