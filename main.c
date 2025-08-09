0470ee459d886ab5711ef3d832d4e51c8409dd6d
David <david.brunner00@gmail.com>
2025-08-09 09:06:23 -0400

    First commit - start of C exercises project

---
+++ main.c
@@ -0,0 +1,21 @@
+//Note to next session:
+//Working on Chapter 3.5 Loops - While and For
+//I skipped the 2.9 and 2.10 exercises, but those would be good for understanding how to work with bits in C
+
+#include <stdio.h>
+
+int main(){
+
+    int bit1 = 0;
+    int bit2 = 2;
+
+    printf("%d\n", bit2<<2);
+    printf("%d", bit1 && bit2);
+
+}
+
+//Exercises to return to
+//3.3 else-if statements
+//sorting array algorithm
+//binary search algorithm
+//hexdump
