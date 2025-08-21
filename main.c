348807c8be606ee9258ec6c42bcd8df5f7c1de2e
David <david.brunner00@gmail.com>
2025-08-20 22:50:13 -0400

    Personal Exercise: Bubble Sort Function

--- main.c
+++ main.c
@@ -1,44 +1,46 @@
 //Note to next session:
-//Working on Chapter 4 - Functions and Program Structure
+//Working on Chapter 4.1 Basics of Functions
 //Chapter 2.10: Exercise 2-9. 
-/* In a two's compliment system, x &= (x-1) deletes the 
-rightmost 1-bit in x. Explain why. Use this observation 
-to write a faster version of bitcount.
-
-Explanation: it deletes the rightmost bit because if its
-odd, then removing 1 makes if the number is even, then 
-removing 1 makes it odd, but anding it with an even number
-removes the 1 at the end
-
-not going to do the exervise cause I have no idea how to 
-make it faster using this knowledge
+/* Sorting Array algorithm
 */
 
-#include <stdio.h>
+int sort(int array[], int length);
 
-unsigned bitcount(unsigned x);
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
 
 int main(){
+    int myArray[] = {12, 2423, 34523, 56536, 44, 14, 6, 761, 2, 4, 68, 983, 3};
 
-    int x = 143014; 
+    sort(myArray, 13);
 
-    //Expected result 143014 -> 107242
-    printf("number: %d \nnumber of 1s in its bit rep: %d", x, bitcount(x));
+    for(int i = 0; i < 13; i++){
+        printf("%d\n", myArray[i]);
+    }
+    
+}
 
-} 
+int sort(int array[], int length){
+    //bubble sort
+    int temp;
 
-unsigned bitcount(unsigned x){
-    int b;
-    for(b = 0; x != 0; x >>= 1)
-        if(x & 01)
-            b++;
-    return b;
+    for (int i = 0; i < length; i++){
+        for (int j = 0; j < length; j++){
+            if(array[i] > array[j]){
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }  
+        }
+    }
+    
 }
 
 //2.9 Exercises : DONE
-//2.10 Exercises
+//2.10 Exercises : PARTIALLY DONE
 //Exercises to return to
-//3.3 else-if statements
+//3.3 else-if statements 
 //sorting array algorithm
 //binary search algorithm
 //hexdump
