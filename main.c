b1a7e880d4bc311847110abc3b6c02a21cd1892b
David <david.brunner00@gmail.com>
2025-08-25 22:08:16 -0400

    Chapter 4.2 Example

--- main.c
+++ main.c
@@ -1,49 +1,53 @@
 //Note to next session:
-//Working on Chapter 4.1 Basics of Functions
-//Chapter 2.10: Exercise 2-9. 
-/* Sorting Array algorithm
+//Working on Chapter 4.2 Functions Returning Non-Integers
+/* Chapter 4.2 Example
+not only must a function have its return type declared, the 
+calling routine must know what type the function is going
+to give. what this means is that if we compile the function
+separately from the main file, and the main file expects an
+int from the function, we get bad juju.
 */
 
-int sort(int array[], int length);
-
+#include <ctype.h>
 #include <stdio.h>
-#include <string.h>
-#include <limits.h>
 
-int main(){
-    int myArray[] = {12, 2423, 34523, 56536, 44, 14, 6, 761, 2, 4, 68, 983, 3};
+//atof: convert string s to double
+double atof(char s[]){
 
-    sort(myArray, 13);
+    double val, power;
+    int i, sign;
 
-    for(int i = 0; i < 13; i++){
-        printf("%d\n", myArray[i]);
+    for (i = 0; isspace(s[i]); i++)
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.')
+        i++;
+    for (power = 1.0; isdigit(s[i]); i++){
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10;
     }
-    
+    return sign * val / power; 
 }
 
-int sort(int array[], int length){
-    //bubble sort
-    int temp;
+int main(){
+    char number[] = "-12034";
 
-    for (int i = 0; i < length; i++){
-        for (int j = 0; j < length; j++){
-            if(array[i] > array[j]){
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }  
-        }
-    }
-    
+    printf("%f", atof(number));
 }
 
 //2.9 Exercises : DONE
 //2.10 Exercises : PARTIALLY DONE
 //Exercises to return to
 //3.3 else-if statements 
-//sorting array algorithm
+//sorting array algorithm : DONE
 //binary search algorithm
 //hexdump
 //3.5 shell sort
 //3.5 exercises
 //3.5 reverse function
+//4.1 rewrite strindex(s,t)
+//4.2 rewrite atof to handle sci notation
