23d9d4f9473a0fbbfd4ee073b90f738ee1e7f98d
David <david.brunner00@gmail.com>
2025-09-09 21:41:05 -0400

    Chapter 4.3 External Variables
    Chapter example implementing a polish notation calculator using external variables to define and access stack and buffer data types

--- main.c
+++ main.c
@@ -1,43 +1,120 @@
 //Note to next session:
-//Working on Chapter 4.2 Functions Returning Non-Integers
-/* Chapter 4.2 Example
-not only must a function have its return type declared, the 
-calling routine must know what type the function is going
-to give. what this means is that if we compile the function
-separately from the main file, and the main file expects an
-int from the function, we get bad juju.
+//Working on Chapter 4.3 External Variables
+/* Chapter 4.3 Example
 */
 
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-//atof: convert string s to double
-double atof(char s[]){
+#define MAXOP 100  //max size of operator/operand
+#define NUMBER '0' //signal that a number was found
 
-    double val, power;
-    int i, sign;
+int getop(char []);
+void push(double);
+double pop(void);
 
-    for (i = 0; isspace(s[i]); i++)
-        ;
-    sign = (s[i] == '-') ? -1 : 1;
-    if (s[i] == '+' || s[i] == '-')
-        i++;
-    for (val = 0.0; isdigit(s[i]); i++)
-        val = 10.0 * val + (s[i] - '0');
-    if (s[i] == '.')
-        i++;
-    for (power = 1.0; isdigit(s[i]); i++){
-        val = 10.0 * val + (s[i] - '0');
-        power *= 10;
-    }
-    return sign * val / power; 
-}
-
+//reverse polish calculator
 int main(){
-    char number[] = "-12034";
+    int type;
+    double op2;
+    char s[MAXOP];
 
-    printf("%f", atof(number));
+    while((type = getop(s)) != EOF){
+        switch(type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("Error: zero divisor\n");
+            case '\n':
+                printf("\t%.8g\n",pop());
+                break;
+            default:
+                printf("Error: unknown command %s\n", s);
+                break;
+        }
+    }
+    return 0;
 }
+
+#define MAXVAL 100 //max depth of val stack
+
+int sp = 0;         //next free stack position
+double val[MAXVAL]; //value stack
+
+//push: push f onto value stack
+void push(double f){
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("Error: stack full, cant push %g\n", f);
+}
+
+//pop: pop and return top value from stack
+double pop(void){
+    if (sp > 0)
+        return val[--sp];
+    else{
+        printf("Error: stack empty\n");
+        return 0.0;
+    }
+}
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+//getop: get next character of numeric operand
+int getop(char s[]){
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;  //not a number
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+#define BUFSIZE 100
+char buf[BUFSIZE];  //buffer for ungetch  
+int bufp = 0;       //next free position in buf
+
+int getch(void){
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else    
+        buf[bufp++] = c;
+}
+
 
 //2.9 Exercises : DONE
 //2.10 Exercises : PARTIALLY DONE
