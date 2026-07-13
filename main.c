fcd4ccd8402e48cab7c054e015511d65a5a0a1bf
Fi-Loy <david.brunner00@gmail.com>
2026-07-13 10:29:25 -0400

    Chapter 5.8: Initialization of Pointer Arrays
    Used a array of pointer characters to store the name of months in a function that returns a char array based on the number passed to the arguments

--- main.c
+++ main.c
@@ -1,45 +1,24 @@
 //Note to next session:
-//Working on Chapter 
+//Working on Chapter 5.8 Initialization of Pointer Arrays
 
 #include <stdio.h>
 
-static char daytab[2][13] = {
-    {0,31,28,31,30,31,30,31,31,30,31,30,31},
-    {0,31,29,31,30,31,30,31,31,30,31,30,31},
-};
+//month name: return name of n-th month
+char *month_name(int n) {
+    static char* name[] = {
+        "Illegal month",
+        "January", "February", "March",
+        "April", "May", "June", 
+        "July", "August", "September",
+        "October", "November", "December"
+    };
 
-//day_of_year: set day of year from month & day
-int day_of_year(int year, int month, int day) {
-    int i, leap;
-    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-    for (i = 1; i < month; i++)
-        day += daytab[leap][i];
-    return day;
-}
-
-//month_day: set month, day from day of year
-void month_day(int year, int yearday, int *pmonth, int *pday) {
-    int i, leap;
-    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
-    *pmonth = i;
-    *pday = yearday;
+    return (n < 1 || n > 12) ? name[0] : name[n];
 }
 
 int main() {
-    int month = 5; //May
-    int day = 22;
-    int year = 2000;
-
-    int bday_of_year = day_of_year(year, month, day);
-    printf("Birthday as day of year: %d\n", bday_of_year);
-
-    int* pmonth;
-    int* pday;
-    month_day(year, bday_of_year, pmonth, pday);
-    printf("Convert back to month: %d, and day: %d\n", *pmonth, *pday);
-
+    printf("The 5th month: %s\n", month_name(5));
+    printf("The 10th month: %s\n", month_name(10));
 }
 
 //2.9 Exercises : DONE
