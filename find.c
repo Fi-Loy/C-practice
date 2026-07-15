731024b2776e97b5a5135e611cd97a79932a0f25
Fi-Loy <david.brunner00@gmail.com>
2026-07-15 12:07:54 -0400

    Chapter 5.10 Command-line Arguments: find example v2
    A more complicated take on the previous find example. Includes handling for optional arguments specified by a '-' sign

--- find.c
+++ find.c
@@ -4,20 +4,40 @@
 
 int myGetline(char *line, int max);
 
-//find: print lines that match pattern from 1st arg
+//find: print lines that match pattern from 1st arg: 2nd version
 main(int argc, char* argv[]) {
     char line[MAXLINE];
-    int found = 0;
+    long lineno = 0;
+    int c, except = 0, number = 0, found = 0;
 
-    if (argc != 2)
-        printf("Usage: find pattern\n");
+    while (--argc > 0 && (*++argv)[0] == '-')
+        while (c = *++argv[0])
+            switch (c) {
+            case 'x':
+                except = 1;
+                break;
+            case 'n':
+                number = 1;
+                break;
+            default:
+                printf("find: illegal option %c\n", c);
+                argc = 0;
+                found = -1;
+                break;
+            }
+    if (argc != 1)
+        printf("Usage: find -x -n pattern\n");
     else
-        while (myGetline(line, MAXLINE) > 0)
-            if (strstr(line, argv[1]) != NULL) {
+        while (myGetline(line, MAXLINE) > 0) {
+            lineno++;
+            if ((strstr(line, *argv) != NULL) != except) {
+                if (number)
+                    printf("%ld:", lineno);
                 printf("%s", line);
                 found++;
             }
-    return found;
+        }  
+    return found;      
 }
 
 /* myGetline:  read a line into s, return length  */ 
