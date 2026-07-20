bb77eb01597f5da22a77b455ab3f9769fa73f770
Fi-Loy <david.brunner00@gmail.com>
2026-07-20 09:39:52 -0400

    6.1 Basics of Structures
    No big example, just following along with the basic code snippets and printing the restults

--- main.c
+++ main.c
@@ -1,79 +1,29 @@
 //Note to next session:
-//Working on Chapter 5.11 Pointers to Functions
+//Working on Chapter 6.1 Basics of Structures
 
 #include <stdio.h>
-#include <string.h>
-#include "hmHelpers.c"
+#include <math.h>
 
-#define MAXLINES 5000 //max num lines to be sorted
-char *lineptr[MAXLINES]; //pointers to text lines
+struct point {
+    int x;
+    int y;
+};
 
-int readlines(char* lineptr[], int nlines);
-void writelines(char* lineptr[], int nlines);
+struct point pt = {3, 4};
+struct point maxpt = {320, 200};
 
-void myQsort(void* lineptr[], int left, int right, int (*comp)(void * , void *));
-int numcmp(const char *,const char *);
+struct rect{ 
+    struct point pt1;
+    struct point pt2;
+};
 
-//sort input lines
-int main(int argc, char* argv[]) {
-    
-    int nlines; //number of input lines read
-    int numeric = 0; // 1 if numeric sort
+int main() {
+    printf("%d, %d\n", pt.x, pt.y);
 
-    if (argc > 1 && strcmp(argv[1], "-n") == 0)
-        numeric = 1;
-    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-        myQsort((void**) lineptr, 0, nlines - 1, (int (*)(void*, void*))(numeric ? numcmp : strcmp));
-        writelines(lineptr, nlines); 
-        return 0;
-    }
-    else {
-        printf("input too big to sort\n");
-        return 1;
-    }
+    double dist;
+    dist = sqrt((double)pt.x * pt.x + (double)pt.y * pt.y);
+    printf("Point distance from origin: %f\n", dist);
 
-}
-
-//qsort: sort v[left]...v[right] into increasing order
-void myQsort(void *v[], int left, int right, int (*comp) (void *, void *)) {
-    int i, last;
-
-    void swap(void *v[], int, int);
-
-    if (left > right)
-        return;
-    swap(v, left, (left + right) / 2);
-    last = left;
-    for (i = left + 1; i <= right; i++) 
-        if ((*comp) (v[i], v[left]) < 0)
-            swap(v, ++last, i);
-    swap(v, left, last);
-    myQsort(v, left, last - 1, comp);
-    myQsort(v, last + 1, right, comp);
-}
-
-#include <stdlib.h>
-
-//numcmp: compare s1 and s2 numerically
-int numcmp(const char* s1,const char* s2) {
-    double v1, v2;
-    v1 = atof(s1);
-    v2 = atof(s2);
-    if (v1 < v2)
-        return -1;
-    else if (v1 > v2)
-        return 1;
-    else
-        return 0;
-}
-
-//sawp:swap the position of two pointers
-void swap(void *v[], int i, int j) {
-    void* temp;
-
-    temp = v[i];
-    v[i] = v[j];
-    v[j] = temp;
 }
 
 //2.9 Exercises : DONE
@@ -103,4 +53,4 @@ void swap(void *v[], int i, int j) {
 //5.6 rewrite functions from earlier chapter with pointers instead of indexing
 //5.10 rewrite the program expr, which can calculate a polish expression from the command line
 //5.11 write the program tail, which prints the last n lines of input and can handle an optional argument "-n"
-//5.14 to 5.17
+//5.14 to 5.17 adding various command line flags for futher functionality
